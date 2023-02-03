236c59a94e2cc4de8558c187621a877813ec63eb
Marcus Grant <marcusfg@gmail.com>
2023-02-03 16:40:14 +0100

    Add script to rm all traces of the notes submodule

---
+++ scripts/rm-module.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+sublocation='site'
+submodule_name="${sublocation}/notes"
+
+# Remove the submodule entry from .git/config file
+git config -f --quiet .git/config --remove-section submodule.${submodule_name}
+
+# Remove the submodule entry from .gitmodules file
+rm -f .gitmodules
+
+# Remove the cached information about the module files
+git rm --cached --quiet ${submodule_name}
+
+# Delete relevant git objects
+rm -rf .git/modules/${submodule_name}
+rm -rf .git/modules/${site}
