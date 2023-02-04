984b7fcb0cf87a494e71d9a21f32e5e91bb1cc7e
Marcus Grant <marcusfg@gmail.com>
2023-02-04 15:22:50 +0100

    Attempt to fix CF deploy by changing scripts
    * Attempt to fix #3

--- scripts/get-notes.sh
+++ scripts/get-notes.sh
@@ -12,10 +12,15 @@ fi
 ME='marcus-grant'
 NOTES_URL="https://${ME}:${NOTES_GH_TOKEN}@github.com/${ME}/notes.git"
 
-if [ ! -f .gitmodules ]; then
-    printf '[submodule "site/notes"]\n' >> .gitmodules
-    printf '    path = ./site/notes\n' >> .gitmodules
-    printf "    url = $NOTES_URL\n" >> .gitmodules
-fi
+# if [ ! -f .gitmodules ]; then
+#     printf '[submodule "site/notes"]\n' >> .gitmodules
+#     printf '\tpath = site/notes\n' >> .gitmodules
+#     printf "\turl = $NOTES_URL\n" >> .gitmodules
+# fi
 
-git submodule foreach git pull
+echo "Adding notes submodule..."
+echo "Has URL of $NOTES_URL"
+
+git submodule add $NOTES_URL site/notes
+
+# git submodule foreach git pull
