0f571de2e909b1bd79159f118b99f90a85908d89
Marcus Grant <marcusfg@gmail.com>
2023-03-11 22:10:46 +0100

    Move global 'meta' data to ./site/site.11tydata.js

---
+++ site/site.11tydata.js
@@ -0,0 +1,10 @@
+module.exports = {
+  layout: 'main.njk',
+  meta: {
+    url: process.env.URL || 'http://localhost:8080',
+    siteName: 'Markipedia',
+    siteDescription: ('Marcus Grant\'s personal Wikipedia, '
+      + 'built with Eleventy & the Zettelkasten method.'),
+    authorName: 'Marcus Grant',
+  },
+};
