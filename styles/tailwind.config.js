7d4f0314f2ee91fc3d1c7b926a0e4f49b02fa4f8
Marcus Grant <marcusfg@gmail.com>
2023-03-12 16:35:30 +0100

    Extend typography plugin theme for prose elements
    * headers have wiki like style
    * code and pre are a little closer to wiki
    * blockquote is a little closer to wiki
    * lists are a little closer to wiki
    * tables are a little closer to wiki
        * More work is needed to make tables fill width from center
    * links aren't exactly like wiki
      * they're now dark blue
      * dark indigo when visited
      * light blue when hovered

--- styles/tailwind.config.js
+++ styles/tailwind.config.js
@@ -1,3 +1,6 @@
+const colors = require('tailwindcss/colors');
+const defaultTheme = require('tailwindcss/defaultTheme');
+
 module.exports = {
   content: [
     // 'site/**/*.html',
@@ -6,12 +9,89 @@ module.exports = {
     './site/index.njk',
   ],
   safelist: [],
+  plugins: [
+    require('@tailwindcss/typography'),
+  ],
   theme: {
-    // extend: {
-    //   colors: {
-    //     change: 'transparent',
-    //   },
-    // },
+    fontFamily: {
+      markipediaSerif: ['Linux Libertine', 'Georgia', 'Times', 'serif'],
+      markipediaSans: 'sans-serif',
+    },
+    colors: {
+      transparent: 'transparent',
+      current: 'currentColor',
+      black: colors.black,
+      gray: colors.gray,
+      white: colors.white,
+      green: colors.green,
+      emerald: colors.emerald,
+      blue: colors.blue,
+      indigo: colors.indigo,
+      yellow: colors.yellow,
+    },
+    extend: {
+      typography: (theme) => ({
+        DEFAULT: {
+          css: {
+            'h1,h2,h3,h4,h5,h6': {
+              fontFamily: ['sans-serif'],
+              fontWeight: theme('fontWeight.700'),
+            },
+            'h1,h2': {
+              fontFamily: ['Linux Libertine', 'Georgia', 'Times', 'serif'],
+              fontWeight: theme('fontWeight.400'),
+              borderBottom: (
+                theme('borderWidth.DEFAULT') + ' '
+                + theme('borderStyle.solid') + ' '
+                + theme('colors.gray.200')
+              ),
+            },
+            'a': { 'color': theme('colors.blue.900') },
+            'a:hover': { 'color': theme('colors.blue.600') },
+            'a:visited': { 'color': theme('colors.indigo.900') },
+            'ul > li::marker': {
+              color: theme('colors.gray.500'),
+            },
+            'code::before': {
+              content: 'none', // remove default backtick ` psuedo-element
+            },
+            'code::after': {
+              content: 'none', // remove default backtick ` psuedo-element
+            },
+            'code': {
+              backgroundColor: theme('colors.gray.200'),
+              borderWidth: theme('borderWidth.DEFAULT'), 
+              borderColor: theme('colors.gray.300'),
+              borderStyle: theme('borderStyle.solid'),
+              paddingTop: theme('padding.0'),
+              paddingBottom: theme('padding.0'),
+              paddingLeft: theme('padding.1'),
+              paddingRight: theme('padding.1'),
+              borderRadius: theme('borderRadius.DEFAULT'),
+              // TODO: Figure out how to get this to work
+              // fontFamily: theme('fontFamily.markipediaMono'),
+              // fontFamily: ['Fira Code', 'monospace'],
+              // fontFamily: ['monospace'],
+            },
+            'pre': {
+              backgroundColor: theme('colors.gray.100'),
+              color: theme('colors.gray.900'),
+              borderWidth: theme('borderWidth.DEFAULT'),
+              borderColor: theme('colors.gray.200'),
+              borderStyle: theme('borderStyle.solid'),
+            },
+            'blockquote': {
+              fontStyle: 'italic',
+              fontFamily: ['Linux Libertine', 'Georgia', 'Times', 'serif'],
+              backgroundColor: theme('colors.gray.100'),
+              color: theme('colors.gray.900'),
+              borderWidth: theme('borderWidth.DEFAULT'),
+              borderColor: theme('colors.gray.200'),
+              borderStyle: theme('borderStyle.solid'),
+            },
+          },
+        }
+      }),
+    },
   },
-  plugins: [],
 };
